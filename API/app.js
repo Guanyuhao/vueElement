8181eb2b92be83dbd6793531271301b41d7b58d3
guanyuhao <272949902@qq.com>
2018-01-12 13:40:02 +0800

    :fire:Patch

--- API/app.js
+++ API/app.js
@@ -22,8 +22,9 @@ var session = require('express-session');
 var app = express();
 
 // 跨域交给webpack处理
-app.all('*', function(req, res, next) {    
-  res.header("Access-Control-Allow-Origin", "*");    
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials',true)
+  res.header("Access-Control-Allow-Origin", "http://192.168.111.6:8080");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");    
   res.header("X-Powered-By",' 3.2.1')    
--- API/routes/kline.js
+++ API/routes/kline.js
@@ -3,10 +3,26 @@ var router = express.Router();
 var request = require('request');
 
 
+router.get('/v1/depth',function(req,res){
+    var params = req.query
+    var market = params.market
+    var size = params.size
+    var timestamp = params.timestamp || 1515647351705 
+    request('http://api.zb.com/data/v1/depth?market='+market+'&size='+size+'&timestamp='+timestamp, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        res.send(body)
+        
+    });
+})
+
 router.get('/v1/kline',function(req,res){
     var params = req.query
     var market = params.market
-    request('http://api.zb.com/data/v1/kline?market='+market, function (error, response, body) {
+    var size = params.size
+    var timestamp = params.timestamp || 1515647351705 
+    request('http://api.zb.com/data/v1/kline?market='+market+'&size='+size+'&timestamp='+timestamp, function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
